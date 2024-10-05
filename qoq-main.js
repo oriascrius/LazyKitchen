362bf38281ec1f48f6fad239042b6c3ae2942e1e
oriascrius <qoq77416416@gmail.com>
2024-10-05 21:01:21 +0800

    qoq，補充商品

--- qoq-main.js
+++ qoq-main.js
@@ -1,31 +1,31 @@
-import './assets/scss/all.scss';
-import './assets/scss/pages/shoppingcart.scss';
-import './assets/scss/pages/productOverview.scss';
-import './assets/scss/pages/feature.scss';
+import "./assets/scss/all.scss";
+import "./assets/scss/pages/shoppingcart.scss";
+import "./assets/scss/pages/productOverview.scss";
+import "./assets/scss/pages/feature.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 
-
+// 回到按鈕上方
 $(document).ready(function () {
-    const $backToTop = $('.backTotop');
+  const $backToTop = $(".backTotop");
 
-    $(window).scroll(function () {
-        const windowScrollTop = $(this).scrollTop(); // 当前滚动高度
-        const documentHeight = $(document).height(); // 文件总高度
-        const windowHeight = $(this).height(); // 视窗高度
+  $(window).scroll(function () {
+    const windowScrollTop = $(this).scrollTop(); // 取得當前滾動高度
+    const documentHeight = $(document).height(); // 取得文件總高度
+    const windowHeight = $(this).height(); // 取得視窗高度
 
-        // 判断是否需要显示按钮
-        if (windowScrollTop > documentHeight / 2 - windowHeight / 2) {
-            $backToTop.fadeIn();
+    // 判斷是否需要顯示按鈕
+    if (windowScrollTop > documentHeight / 2 - windowHeight / 2) {
+      $backToTop.fadeIn();
 
-            // 设置 right 值根据窗口宽度
-            const rightValue = $(this).width() >= 992 ? '40px' : '24px';
-            $backToTop.css({ right: rightValue, top: '40px' }); // 固定 top 为 40px
-        } else {
-            $backToTop.fadeOut(); // 隐藏按钮
-        }
-    });
+      // 根據視窗寬度設置 right 值
+      const rightValue = $(this).width() >= 992 ? "40px" : "24px";
+      $backToTop.css({ right: rightValue, top: "40px" }); // 固定 top 為 40px
+    } else {
+      $backToTop.fadeOut(); // 隱藏按鈕
+    }
+  });
 
-    $backToTop.click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 100); // 返回顶部
-    });
+  $backToTop.click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 100); // 返回頂端
+  });
 });
