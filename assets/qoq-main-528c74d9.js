8881657854d9c46413c28afc83230377161a7681
oriascrius <qoq77416416@gmail.com>
2024-10-18 10:53:15 +0800

    Updates

--- assets/qoq-main-528c74d9.js
+++
@@ -1,1 +0,0 @@
-import"./bootstrap.min-487c5bde.js";document.addEventListener("DOMContentLoaded",function(){AOS.init({easing:"ease",duration:400})});
---
+++ assets/qoq-main-c1e83112.js
@@ -0,0 +1,1 @@
+import"./bootstrap.min-2f3a45d8.js";document.addEventListener("DOMContentLoaded",function(){AOS.init({easing:"ease",duration:400})});
