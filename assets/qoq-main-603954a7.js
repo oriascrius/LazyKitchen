93c010015632eecdb3c1603e1439b64ef9b9559f
oriascrius <qoq77416416@gmail.com>
2024-10-19 23:06:02 +0800

    Updates

--- assets/qoq-main-603954a7.js
+++
@@ -1,1 +0,0 @@
-import"./bootstrap.min-e7121c8d.js";document.addEventListener("DOMContentLoaded",function(){AOS.init({easing:"ease",duration:400})});
---
+++ assets/qoq-main-859b94c6.js
@@ -0,0 +1,1 @@
+import"./bootstrap.min-63b81767.js";document.addEventListener("DOMContentLoaded",function(){AOS.init({easing:"ease",duration:400})});
